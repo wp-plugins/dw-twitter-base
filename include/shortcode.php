8d24f1cf96684e23d9fd9752845516a14f60d224
simone.marchese <simone.marchese@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-15 17:19:56 +0000

    shortcode update
    git-svn-id: https://plugins.svn.wordpress.org/dw-twitter-base/trunk@1113288 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/shortcode.php
+++ include/shortcode.php
@@ -4,38 +4,9 @@
 # Shortcode Twitter Base - Insert Username twitter
 if (!defined('ABSPATH')) die("Accesso diretto al file non permesso");
 
-function twitter_shortcode($atts) {
-  
-  $consumerkey = get_option('dw_consumerkey');		
-  $consumersecret = get_option('dw_consumersecret');
-  $accesstoken = get_option('dw_accesstoken');
-  $accesstokensecret = get_option('dw_accesstokensecret');
-  $twitterusername = get_option('dw_twitteruser');
-  
-  extract(shortcode_atts(array(
-      'username' => $twitterusername,
-      'num' => 0,
-   ), $atts));
-   
-   if($num==0) $num = 1;
-   
-
-   $output = "";
-   $output .= "<br />";
-   $connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
-   $tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$username."&count=".$num);	
-			 
-	for($i=0;$i<$num;$i++){
-		$output .= "<div class='twitter-list'>".getURL($tweets[$i]->text)."</div>";	
-	}
-	return $output;
-}
-
-add_shortcode('twitter', 'twitter_shortcode');
-
 # Shortcode Twitter Base - Username set 
 
-function twb_user_shortcode($atts) {
+function twb_shortcode($atts) {
   
   $consumerkey = get_option('dw_consumerkey');		
   $consumersecret = get_option('dw_consumersecret');
@@ -63,4 +34,4 @@ function twb_user_shortcode($atts) {
 	return $output;
 }
 
-add_shortcode('twitter_user', 'twb_user_shortcode');
+add_shortcode('twitter', 'twb_shortcode');
